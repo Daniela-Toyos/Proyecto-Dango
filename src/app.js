4b6a6e5a34ce5a328a7bfc5c938dc7f1ae368a6f
DanielaToyos <danietob8@gmail.com>
2024-11-14 04:07:44 -0300

    arreglando estilos con tailwind parte 2

--- src/app.js
+++ src/app.js
@@ -1,13 +1,14 @@
-const swiper = new Swiper('.swiper-container', {
-    slidesPerView: 3, 
-    spaceBetween: 30,
-    pagination: {
-      el: '.swiper-pagination',
-      clickable: true,
-    },
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
+window.addEventListener('load', () => {
+    const swiper = new Swiper('.swiper-container', {
+      loop: true,
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+      autoplay: {
+        delay: 5000,
+        disableOnInteraction: false,
+      },
+    });
   });
   
