290da8b4e9d9b673d8d4a3edc9ecfaee22f96127
DanielaToyos <danietob8@gmail.com>
2024-11-15 01:47:35 -0300

    se terminaron estilos y ahora se comienza logica en js

--- src/app.js
+++ src/app.js
@@ -1,14 +1,35 @@
 window.addEventListener('load', () => {
-    const swiper = new Swiper('.swiper-container', {
+    const heroSwiper = new Swiper('.hero-carousel', {
       loop: true,
       navigation: {
         nextEl: '.swiper-button-next',
         prevEl: '.swiper-button-prev',
       },
       autoplay: {
-        delay: 5000,
+        delay: 7000,
         disableOnInteraction: false,
       },
+
+      allowSlideNext: true,
+      allowSlidePrev: true,
+    
     });
-  });
+
+      // Carrusel de Featured Collection (sin autoplay)
+      const featuredCollectionSwiper = new Swiper('.featured-collection-carousel', {
+      loop: true,
+      slidesPerView: 1, // Cantidad de slides visibles
+      spaceBetween: 20, // Espacio entre slides
+      navigation: {
+        nextEl: '.featured-next',
+        prevEl: '.featured-prev',
+      },
+      autoplay: false, // No se mueve automáticamente
+    });
+
+    let carrito = document.getElementById("botonCarrito"); 
+    
+
+
+});
   
